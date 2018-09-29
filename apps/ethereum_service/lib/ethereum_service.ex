10e6b0b21cfc59ad66602c1d252efd0805705e95
douglasgabriel <douglasxgabriel@gmail.com>
2018-09-28 21:09:32 -0300

    init app ethereum_service

---
+++ apps/ethereum_service/lib/ethereum_service.ex
@@ -0,0 +1,18 @@
+defmodule EthereumService do
+  @moduledoc """
+  Documentation for EthereumService.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EthereumService.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
